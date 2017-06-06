f491cb605417285de510e49ce4c358918a16faf4
tylerhuntington222 <tylerhuntington222@gmail.com>
2017-06-06 10:44:45 -0700

    updated weighed_centroids IO script

--- R_code/weighted_centroids_IO.R
+++ R_code/weighted_centroids_IO.R
@@ -25,6 +25,8 @@
 
 # a basic function to get the filepath of the current script
 csf <- function() {
+  # install packages
+  install.packages("rstudioapi")
   # adapted from http://stackoverflow.com/a/32016824/2292993
   cmdArgs = commandArgs(trailingOnly = FALSE)
   needle = "--file="
@@ -50,23 +52,28 @@ csf <- function() {
   }
 }
 
+###### SET WORKING DIRECTORY ######
+this.dir <- dirname(csf())
+setwd(this.dir)
+rm(list=ls())
+
 ###### SOURCE LOCAL FUNCTIONS #######
 source("weighted_centroids_fun.R")
 
 
-# ###### INSTALL PACKAGES IF NECCESSARY ######
-# install.packages("raster")
-# install.packages("spdep")
-# install.packages("maptools")
-# install.packages("rgdal")
-# install.packages("plyr")
-# install.packages("geosphere")
-# install.packages("rgeos")
-# install.packages("raster")
-# install.packages("spdep")
-# install.packages("maptools")
-# install.packages("rgdal")
-# install.packages("plyr")
+###### INSTALL PACKAGES IF NECCESSARY ######
+install.packages("raster")
+install.packages("spdep")
+install.packages("maptools")
+install.packages("rgdal")
+install.packages("plyr")
+install.packages("geosphere")
+install.packages("rgeos")
+install.packages("raster")
+install.packages("spdep")
+install.packages("maptools")
+install.packages("rgdal")
+install.packages("plyr")
 
 
 ###### LOAD LIBRARIES ######
@@ -84,7 +91,7 @@ library(rgdal)
 library(plyr)
 
 # start timer
-system.time({
+#system.time({
   
 ###### LOAD DATA ######
 
@@ -92,30 +99,34 @@ system.time({
 counties.spdf <- readRDS("../clean_binary_data/counties.spdf.RDS")
 
 # load NLCD raster
-nlcd.ras <- 
+nlcd <- 
   raster("../../raw_data_files/nlcd_2011_landcover_2011_edition_2014_10_10.img")
 
 # TEMP: crop counties layer to Iowa for testing functionality
 #counties.spdf <- subset(counties.spdf, counties.spdf$STATENAME == "IOWA")
 
-# TEMP: crop counties layer to US Midwest states only
-midwest.states <- c("IL", "IN", "IA", "KS", "MI", 
-                    "MN", "MS", "NE", "ND", 
-                    "OH", "SD", "WI")
+# # TEMP: crop counties layer to US Midwest states only
+# midwest.states <- c("IL", "IN", "IA", "KS", "MI", 
+#                     "MN", "MS", "NE", "ND", 
+#                     "OH", "SD", "WI")
+# 
+# counties.spdf <- subset(counties.spdf, 
+#                         counties.spdf$STATEABBREV %in% midwest.states)
 
+# TEMP: crop counties layer to ND
 counties.spdf <- subset(counties.spdf, 
-                        counties.spdf$STATEABBREV %in% midwest.states)
+                        counties.spdf$STATEABBREV == "ND")
 
 ###### PREP DATA #######
 
 # crop extent of nlcd RasterLayer to extent of US counties layer
-nlcd.ras <- crop(nlcd.ras, counties.spdf)
-
-# make mask raster with binary vals based on crop/pasture (1) or other (0)
-mask <- (nlcd.ras == 81 | nlcd.ras == 82)
+nlcd <- crop(nlcd, counties.spdf)
 
 # create matrix from mask raster layer
-mask <- as.matrix(mask)
+nlcd <- as.matrix(nlcd)
+
+# make mask raster with binary vals based on crop/pasture (1) or other (0)
+mask <- (nlcd == 81 | nlcd == 82)
 
 # re-create raster layer from extracted matrix
 mask <- raster(mask)
@@ -126,13 +137,25 @@ proj4string(mask) <- crs(counties.spdf)
 # set extent of mask to extent of county polys
 extent(mask) <- extent(counties.spdf)
 
+###### COMPUTE TRUE CENTROIDS ######
+t.cents <- gCentroid(counties.spdf, byid = T)
+  
 ###### COMPUTE WEIGHTED CENTROIDS ######
 cntrs.sptdf <- WeightedCentroids(counties.spdf, mask)
 
 # export result as binary data file in output directory
 saveRDS(cntrs.sptdf, "../output/wtd_county_cntroids.sptdf.RDS")
 
-}) # end timer
+#}) # end timer
+
+
+# TEMP: Plots for testing and checking analysis
+plot(counties.spdf)
+plot(mask)
+plot(counties.spdf, add = T)
+plot(t.cents, add = T, col = 'blue')
+plot(cntrs.sptdf, add = T, col = 'red')
+
 
 
 
